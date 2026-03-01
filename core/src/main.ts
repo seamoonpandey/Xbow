5d7717c148e599be898327e03e21df748085e4b4
Seamoon Pandey <pas078bct035@wrc.edu.np>
2026-03-01 23:19:15 +0545

    fix helmet default import for esm compatibility

--- core/src/main.ts
+++ core/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import * as helmet from 'helmet';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
